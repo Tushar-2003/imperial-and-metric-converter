cbb455a064ea02845fe69be811dd50b5bf718367
Tushar Gupta <Tushargupta3002@gmail.com>
2022-06-09 02:41:34 +0530

    added javascript and some markdown

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+# A Simple webpage that lets you converts a given value
 
+**converts the value to** 
+
+ 1. Length(Meters/Feets)
+ 2. Volume(Liters/Gallons)
+ 3. Mass(Kilograms/Pounds)
+   
+   
--- script.js
+++ script.js
@@ -1,1 +1,72 @@
-let value
+// let originalValue = takeValue();
+
+function combine(){
+    showValue()
+    convertValue()   
+}
+
+// function to take value from the user
+function takeValue(){
+    let originalValue = document.getElementById("original-value").value
+    originalValue = parseInt(originalValue)
+    return originalValue
+
+}
+
+// function to show te value
+function showValue() {
+    let originalValue = takeValue() 
+    for (let i = 0; i < 6; i++) {
+        let showValue = document.getElementsByClassName("show-value")[i]
+        showValue.innerHTML = originalValue 
+    }
+
+}
+
+// function to convert
+function convertValue(){
+
+    let a
+    let feets = document.getElementById("feets")
+    let meters = document.getElementById("meters")
+    let gallons = document.getElementById("gallons")
+    let liters = document.getElementById("liters")
+    let pounds= document.getElementById("pounds")
+    let kilos= document.getElementById("kilos")
+
+    // meter to feet
+    a=takeValue()
+    a = a*3.28084
+    a = a.toFixed(3)
+    feets.textContent = a
+
+    // feet to meter
+    a=takeValue()
+    a = a/3.28084
+    a = a.toFixed(3)
+    meters.textContent = a
+
+    // liters to gallons
+    a=takeValue()
+    a = a/3.7854
+    a = a.toFixed(3)
+    gallons.textContent = a
+
+    // gallons to liters
+    a=takeValue()
+    a = a*3.7854
+    a = a.toFixed(3)
+    liters.textContent = a
+
+    // kilos to pounds
+    a=takeValue()
+    a= a*2.20462
+    a = a.toFixed(3)
+    pounds.textContent = a   
+
+    // pounds to kilos
+    a=takeValue()
+    a= a/2.20462
+    a = a.toFixed(3)
+    kilos.textContent = a
+}
